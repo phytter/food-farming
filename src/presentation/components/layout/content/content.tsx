12cd4c3af5b7f4a897a53dae3fa4be89215021c4
phytter <phytter@hotmail.com>
2022-05-23 20:24:32 -0300

    feat: finish content

--- src/presentation/components/layout/content/content.tsx
+++ src/presentation/components/layout/content/content.tsx
@@ -1,7 +1,12 @@
 import { styled } from '@stitches/react';
 
 const Content = styled('div', {
-
+    backgroundColor: '#FFF9F2',
+    height: 'calc(100vh - 200px)',
+    width: '90%',
+    margin: '0 auto',
+    padding: 60,
+    boxShadow: '0px 50px 28px -19px rgba(244,234,226,1)'
 })
 
 export default Content
