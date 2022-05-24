a01f7b03ee0175668623fcafbd3ff35f7ea56c37
phytter <phytter@hotmail.com>
2022-05-23 21:38:30 -0300

    feat: highlighted current seleted menu

--- src/presentation/components/layout/header/header.tsx
+++ src/presentation/components/layout/header/header.tsx
@@ -1,11 +1,16 @@
 import Leaft from '@/assets/leaft.png'
 import { LeaftIcon, MenuItem, StyledHeader } from './styles'
+import { useLocation } from 'react-router-dom'
 
 const Header: React.FC = () => {
+    const location = useLocation()
+    
+    const getColorStateByPath = (path: string): 'selected' | undefined => location.pathname === path ? 'selected' :  undefined
+
     return (
         <StyledHeader>
-            <MenuItem color='selected' href='/'>Home</MenuItem>
-            <MenuItem href='/about'>About</MenuItem>
+            <MenuItem color={getColorStateByPath('/')} href='/'>Home</MenuItem>
+            <MenuItem href='/about' color={getColorStateByPath('/about')}>About</MenuItem>
             <MenuItem>Products</MenuItem>
             <LeaftIcon src={Leaft}/>
             <MenuItem>Recipes</MenuItem>
