2d281ca59a4b2ae9f266818b7dc175e480038e8d
phytter <phytter@hotmail.com>
2022-05-23 22:41:30 -0300

    feat: add redirect to unmatch route

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -3,7 +3,8 @@ import { Home, About } from "@/presentation/pages";
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
 
 export default function App() {
@@ -12,7 +13,11 @@ export default function App() {
         <Routes>
             <Route path="/" element={<Panel><Home /></Panel>} />
             <Route path="/about" element={<Panel><About /></Panel>} />
-            <Route path="*" element={<Panel><Home /></Panel>} />
+            <Route path="/products" element={<Panel><h1>Products</h1></Panel>} />
+            <Route path="/recipes" element={<Panel><h1>Recipes</h1></Panel>} />
+            <Route path="/faq" element={<Panel><h1>FAQ</h1></Panel>} />
+            <Route path="/help" element={<Panel><h1>Help</h1></Panel>} />
+            <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
     </Router>
   );
